9ffde5443e041d36fd614ee043ce4bece998f552
Quentin Monmert <qmonmert@qmonmert.local>
2018-05-10 12:56:34 +0200

    ng generate @angular/material:material-table --name=main-table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatCheckboxModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from "@angular/material";
+import { MatCheckboxModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from "@angular/material";
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MainTableComponent } from './main-table/main-table.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  declarations: [AppComponent, MainNavComponent, MainTableComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule],
   providers: [],
   bootstrap: [AppComponent]
 })
