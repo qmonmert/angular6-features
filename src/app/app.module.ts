daeb9df4331f13bb9aa645479f28d1c3248711fc
Quentin Monmert <qmonmert@qmonmert.local>
2018-05-10 13:00:48 +0200

    ng generate @angular/material:material-dashboard --name=main-dash

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,15 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatCheckboxModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from "@angular/material";
+import { MatCheckboxModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule } from "@angular/material";
 import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MainTableComponent } from './main-table/main-table.component';
+import { MainDashComponent } from './main-dash/main-dash.component';
 
 @NgModule({
-  declarations: [AppComponent, MainNavComponent, MainTableComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  declarations: [AppComponent, MainNavComponent, MainTableComponent, MainDashComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCheckboxModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule, MatGridListModule, MatCardModule, MatMenuModule],
   providers: [],
   bootstrap: [AppComponent]
 })
