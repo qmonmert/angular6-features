04b8ec90516d60b894f4c6fd219371944fe1bf95
Quentin Monmert <qmonmert@qmonmert.local>
2018-05-10 13:34:46 +0200

    mat-tree

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
+import { Component } from "@angular/core";
+import {
+  BreakpointObserver,
+  Breakpoints,
+  BreakpointState
+} from "@angular/cdk/layout";
+import { Observable, of as observableOf } from "rxjs";
+import { NestedTreeControl } from "@angular/cdk/tree";
+
+export type Sport = {
+  name: string;
+  children: Sport[];
+};
 
 @Component({
-  selector: 'main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css']
+  selector: "main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.css"]
 })
 export class MainNavComponent {
-  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(
+    Breakpoints.Handset
+  );
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  sports: Sport[] = [
+    {
+      name: "Triathlon",
+      children: [
+        { name: "Running", children: [] },
+        { name: "Cycling", children: [] },
+        { name: "Swimming", children: [] }
+      ]
+    },
+    {
+      name: "Football",
+      children: []
+    }
+  ];
+
+  treeControl = new NestedTreeControl<Sport>(node =>
+    observableOf(node.children)
+  );
 }
